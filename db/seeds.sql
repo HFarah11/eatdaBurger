9b93d3c5e865491436527142e8629938d5ef2f67
Har <harunkfarah@gmail.com>
2018-09-18 17:42:38 -0400

    fixed bugs

---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO burgers
+  (burger_name, devoured)
+VALUES
+  ("The One with cheese", true),
+  ("The One with meat", false),
+  ("The One with fries", false),
+  ("The One with onions", false),
+  ("The One with chicken", true),
+  ("The One made vegan", true),
+  ("The Canadian One", true),
+  ("The American One", false),
+  ("The One with heartburn", false),
+  ("The Spicy One", false),
+  ("The One with extra mayo", false),
+  ("The One made with love", true),
+  ("The One with with speed", false);
+
+
